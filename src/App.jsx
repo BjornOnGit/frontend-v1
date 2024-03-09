d261383d467d9cbec30a23e6f1b7ac10194a2bf6
Umeh Promise <umehpromise1000@gmail.com>
2024-03-09 18:23:45 +0100

    feat: virtual doctor basic project configuration and setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className='h-screen grid place-content-center'>
+      <h1 className='text-6xl font-bold uppercase'>
+        Welcome to ECE 505 Virtual Doctor Project
+      </h1>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      white: '#FFFFFF',
+      black: '#000000',
+      'color-green-900': '#29E189',
+      'color-blue-100': '#CAF2FF',
+      'color-blue-900': '#00A3FF',
+      'color-purple-900': '#FFCCFD',
+      'color-grey': '#D9D9D9',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
