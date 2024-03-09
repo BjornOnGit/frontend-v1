51691c69dfd9193adc6fd3f5b6f9cbf45da48316
Umeh Promise <umehpromise1000@gmail.com>
2024-03-09 18:44:34 +0100

    feat: added react hook form, react query, axioss and also configured them

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Home from './pages/unAuthPages/Home';
+
 export default function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnReconnect: false,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
   return (
-    <div className='h-screen grid place-content-center'>
-      <h1 className='text-6xl font-bold uppercase'>
-        Welcome to ECE 505 Virtual Doctor Project
-      </h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+      <Toaster
+        position='top-center'
+        gutter={12}
+        containerStyle={{ margin: '0.8rem' }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+            style: { color: '#00C75B' },
+          },
+          error: {
+            duration: 5000,
+            style: { color: '#CB3232' },
+          },
+          style: {
+            fontSize: '1.6rem',
+            maxWidth: '50rem',
+            padding: '1.6rem 2.4rem',
+          },
+        }}
+      />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
---
+++ src/pages/unAuthPages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className='h-screen grid place-content-center'>
+      <h1 className='text-6xl font-bold uppercase text-blue-900'>
+        Welcome to ECE 505 Virtual Doctor Project
+      </h1>
+    </div>
+  );
+}
